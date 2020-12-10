200edecb74be03828d57f60ce0c99dd11362cc73
Andrej Gasic <andrej@gasic.rs>
2020-12-10 13:26:05 +0100

    Integrisanje sa jezgrom

--- src/main/java/com/bosanskilonac/szl/model/Avion.java
+++ src/main/java/com/bosanskilonac/szl/model/Avion.java
@@ -1,4 +1,4 @@
-package model;
+package com.bosanskilonac.szl.model;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/bosanskilonac/szl/model/Let.java
+++ src/main/java/com/bosanskilonac/szl/model/Let.java
@@ -1,4 +1,4 @@
-package model;
+package com.bosanskilonac.szl.model;
 
 import java.time.Duration;
 
